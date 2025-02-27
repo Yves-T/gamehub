ccd97509891d8889cc810f4a985fb5054da44c14
Yves Talboom <yves.talboom@gmail.com>
2025-02-27 07:33:16 +0100

    Display game icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    linux: FaLinux,
+    mac: FaApple,
+    nintendo: SiNintendo,
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    web: BsGlobe,
+    xbox: FaXbox,
+  };
+  return (
+    <>
+      <HStack marginY={"10px"}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color="gray.500" />
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
