59f43ce4f680116b7db507986adf8f277a68d8d3
Yves Talboom <yves.talboom@gmail.com>
2025-03-01 06:08:28 +0100

    Use react query to handle data fetching

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -31,7 +31,7 @@ const GenresList = ({ onSelectGenre, selectedGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
