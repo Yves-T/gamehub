6b62335bf57732cc8848fe5f449de608ed18a13c
Yves Talboom <yves.talboom@gmail.com>
2025-03-02 07:31:52 +0100

    Building game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  description_raw: string;
   background_image: string;
-  parent_platforms: { platform: Platform }[];
+  description_raw: string;
+  genres: Genre[];
+  id: number;
   metacritic: number;
+  name: string;
+  parent_platforms: { platform: Platform }[];
+  publishers: Publisher[];
   rating_top: number;
+  slug: string;
 }
